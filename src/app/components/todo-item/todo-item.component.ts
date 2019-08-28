07b711747df6b69c8bb6bedeace3ceaa82a3035a
Chris <chharvey@icloud.com>
2019-08-28 02:32:55 -0400

    onToggle onDelete

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -20,4 +20,12 @@ export class TodoItemComponent implements OnInit {
     };
     return classes;
   }
+
+  onToggle(todo) {
+    console.log('toggle');
+  }
+
+  onDelete(todo) {
+    console.log('delete');
+  }
 }
